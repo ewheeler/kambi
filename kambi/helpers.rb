6546829191e2680faab2204093d1be1b27943e43
ewheeler <ewheeler@236eb38a-cce2-4ec6-b019-ad969920cdbf>
2008-07-02 22:11:50 +0000

    refactored recaptcha things to use markaby
    git-svn-id: http://trac.mepemepe.com/svn/kambi/branches/development@177 236eb38a-cce2-4ec6-b019-ad969920cdbf

--- kambi/helpers.rb
+++ kambi/helpers.rb
@@ -54,6 +54,8 @@ class Object
 end
 
 module Kambi::Helpers
+  require 'net/http'
+  require 'uri'
   # include Kambi::Controllers
   # include Kambi::Models
   # include Kambi::Views
@@ -121,57 +123,63 @@ module Kambi::Helpers
     key   = options[:public_key] ||= RECAPTCHA_PUBLIC_KEY
     error = options[:error] #||= session[:recaptcha_error]
     uri   = options[:ssl] ? RECAPTCHA_API_SECURE_SERVER : RECAPTCHA_API_SERVER
-    xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
+    out = Markaby::Builder.new 
     if options[:display] 
-      xhtml.script(:type => "text/javascript"){ xhtml.text! "var RecaptchaOptions = #{options[:display].to_json};\n"}
+      out.html do
+      	script(:type => "text/javascript"){ out + text!( "var RecaptchaOptions = #{options[:display].to_json};\n")}
+	end
     end
     if options[:ajax]
-     xhtml.div(:id => 'dynamic_recaptcha') {}
-     xhtml.script(:type => "text/javascript", :src => "#{uri}/js/recaptcha_ajax.js") {}
-     xhtml.script(:type => "text/javascript") do
-       xhtml.text! "Recaptcha.create('#{key}', document.getElementById('dynamic_recaptcha') );"
+      out.html do
+    	div(:id => 'dynamic_recaptcha') {}
+     	script(:type => "text/javascript", :src => "#{uri}/js/recaptcha_ajax.js") {}
+     	script(:type => "text/javascript") do
+        text("Recaptcha.create('#{key}', document.getElementById('dynamic_recaptcha') );")
+	end
      end
     else
-      xhtml.script(:type => "text/javascript", :src => "#{uri}/challenge?k=#{key}&error=#{error}") {}
+      out.html do
+      	script(:type => "text/javascript", :src => "#{uri}/challenge?k=#{key}&error=#{error}") {}
+	end
       unless options[:noscript] == false
-        xhtml.noscript do
-          xhtml.iframe(:src => "#{uri}/noscript?k=#{key}",
+        out.html do
+		noscript do
+           iframe(:src => "#{uri}/noscript?k=#{key}",
                        :height => options[:iframe_height] ||= 300,
                        :width  => options[:iframe_width]  ||= 500,
                        :frameborder => 0) {}; xhtml.br
-          xhtml.textarea(:name => "recaptcha_challenge_field", :rows => 3, :cols => 40) {}
-          xhtml.input :name => "recaptcha_response_field",
-                      :type => "hidden", :value => "manual_challenge"
+           textarea(:name => "recaptcha_challenge_field", :rows => 3, :cols => 40) {}
+           input(:name => "recaptcha_response_field",
+                      :type => "hidden", :value => "manual_challenge")
+	end
         end
       end
     end
-    raise ReCaptchaError, "No public key specified." unless key
-    return out
+#    raise ReCaptchaError, "No public key specified." unless key
+  return out.to_s
   end # recaptcha_tags
   
   
   def verify_recaptcha(model = nil)
 #        return true if SKIP_VERIFY_ENV.include? ENV['RAILS_ENV']
-    raise ReCaptchaError, "No private key specified." unless RECAPTCHA_PRIVATE_KEY
+#    raise ReCaptchaError, "No private key specified." unless RECAPTCHA_PRIVATE_KEY
     begin
       recaptcha = Net::HTTP.post_form URI.parse("http://#{RECAPTCHA_VERIFY_SERVER}/verify"), {
-        :privatekey => ENV['RECAPTCHA_PRIVATE_KEY'],
-        :remoteip   => request.remote_ip,
-        :challenge  => params[:recaptcha_challenge_field],
-        :response   => params[:recaptcha_response_field]
+        :privatekey => RECAPTCHA_PRIVATE_KEY,
+        :challenge  => input.recaptcha_challenge_field,
+        :response   => input.recaptcha_response_field
       }
       answer, error = recaptcha.body.split.map { |s| s.chomp }
       unless answer == 'true'
-        session[:recaptcha_error] = error
-        model.valid? if model
-        model.errors.add_to_base "Captcha response is incorrect, please try again." if model
-        return false
+	@h1 = "Oops"
+        @msg = "Captcha response is incorrect, please try again."
+	@redir = "/posts/" + input.post_id
+	return false
       else
-        session[:recaptcha_error] = nil
         return true
       end
     rescue Exception => e
-      raise ReCaptchaError, e
+      raise e.to_s
     end    
   end # verify_recaptcha
 
